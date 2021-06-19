48537ccac7a0bc9eba25f77199e19436f8da783e
Tiago Aguiar <tiago.aguiar1@hotmail.com>
2021-06-19 15:21:29 -0300

    example using arrow function

---
+++ Exemplo1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintMessage("Example 1");
+
+            Console.WriteLine(ConcatName("Tiago", "Aguiar"));
+        }
+
+        static string ConcatName(string name, string lastname)
+        {
+            return $"{name} {lastname}";
+        }
+
+        static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Exemplo2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exemplo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintMessage("Example 1");
+
+            Console.WriteLine(ConcatName("Tiago", "Aguiar"));
+        }
+
+        static string ConcatName(string name, string lastname)
+            => $"{name} {lastname}";
+
+        static void PrintMessage(string message)
+            => Console.WriteLine(message);
+    }
+}
